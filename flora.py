95abb62fed9c724051d77350a412bbf1c196fbff
andy <andersgee@gmail.com>
2017-06-30 15:31:51 +0200

    25 degree angle

--- flora.py
+++ flora.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from math import pi
 
 def factorial(n, a, p0=np.array([0,0]), p1=np.array([0,1])):
     if n < 1:
@@ -7,13 +8,13 @@ def factorial(n, a, p0=np.array([0,0]), p1=np.array([0,1])):
     else:
         plt.plot([p0[0], p1[0]], [p0[1], p1[1]])
         p2 = np.dot((p1-p0), rotMat(a)) + p1
-        factorial(n-1, pi/7, p1, p2)
-        factorial(n-1, -pi/7, p1, p2)
+        factorial(n-1, angle, p1, p2)
+        factorial(n-1, -angle, p1, p2)
 
 def rotMat(a):
     return np.array([[np.cos(a), np.sin(a)], [-np.sin(a),  np.cos(a)]])
 
 if __name__ == '__main__':
-    pi=3.14159
-    factorial(6, a=-pi/7)
+    angle=25*pi/180
+    factorial(6, -angle)
     plt.show()
